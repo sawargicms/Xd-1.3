ff42565b56fb740331a6947a0b05732d38d72a7e
A-F <sawargi.ciamis@gmail.com>
2023-01-31 02:35:14 +0700

    Copy project files

---
+++ lib/models/navigation_item.dart
@@ -0,0 +1,14 @@
+import 'package:wnrapp_app/enum/action_type.dart';
+
+class NavigationItem {
+  final String name;
+  final String icon;
+  final ActionType type;
+  final String value;
+
+  NavigationItem(
+      {required this.name,
+      required this.icon,
+      required this.type,
+      required this.value});
+}
