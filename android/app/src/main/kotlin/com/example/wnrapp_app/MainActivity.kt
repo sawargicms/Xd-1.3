8ce778dbc10a6c4845c726f2b0d61af2ef318bb7
A-F <sawargi.ciamis@gmail.com>
2023-01-31 03:16:17 +0700

    Copy project files

--- android/app/src/main/kotlin/com/example/wnrapp_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wnrapp_app/MainActivity.kt
@@ -3,4 +3,8 @@ package com.example.wnrapp_app
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-}
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		GeneratedPluginRegistrant.registerWith(this)
+	  }
+	}
